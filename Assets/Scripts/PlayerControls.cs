627b8d7fca10009572d1a30cb4274390cdcc5e3d
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-05-30 19:37:26 +0200

    Mathf.Clamp

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField] float speed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
 
-
-    // Update is called once per frame
     void Update()
     {
-        float hotizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+     
+
+        float xOffset = xThrow * Time.deltaTime * speed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+
+        float yOffset = yThrow * Time.deltaTime * speed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3 (clampedXPos, clampedYPos, transform.localPosition.z);
+
         
     }
 }
