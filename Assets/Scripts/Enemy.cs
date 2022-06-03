f4e548f7ae352e9894d088e9038a32d2be0dbfd4
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-03 20:53:33 +0200

    added explosion effect to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] Transform parent;
+   
+
     void OnParticleCollision(GameObject other)
     {
-
+       GameObject vfx = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;     //klony zrobione wyzej (vfx) zamiast zaœmiecaæ hierarchy s¹ wrzucany do pustego gameobject "parent"
         Destroy(gameObject);
     }
 
-
-
-
+   
 
 
 
