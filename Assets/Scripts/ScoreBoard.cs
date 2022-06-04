7e123374559b449358122f06782bf0ef6a1aedec
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-04 13:15:11 +0200

    Added display score

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     int score;
+    TMP_Text scoreText;
 
+    void Start()
+    {
+       scoreText = GetComponent<TMP_Text>();
+       scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;    //score = score + amountToIncrease
-
-        Debug.Log($"Score is now {score} points");
+        scoreText.text = score.ToString();
 
     }
 
 
-
+       
 }
