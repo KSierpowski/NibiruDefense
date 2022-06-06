e1a4564cbf28ff8853d1daa641d263764e08c803
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-06-06 08:31:45 +0200

    FindWithTag

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int healthPoints = 10;
     [SerializeField] GameObject hitVFX;
 
-     
-     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+    ScoreBoard scoreBoard;
 
 
      void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
         AddRigidbody();
     }
 
@@ -39,14 +39,14 @@ public class Enemy : MonoBehaviour
     void KillEnemy()
     {
         GameObject vfx = Instantiate(explosionVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;     //klony zrobione wyzej (vfx) zamiast zaœmiecaæ hierarchy s¹ wrzucany do pustego gameobject "parent"  
+        vfx.transform.parent = parentGameObject.transform;     //klony zrobione wyzej (vfx) zamiast zaœmiecaæ hierarchy s¹ wrzucany do pustego gameobject "parent"  
         Destroy(gameObject);
     }
 
     void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         healthPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
         
